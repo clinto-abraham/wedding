edd5e4a186aaa252ab0dbd66b299b8079b2f76e7
Clinto Abraham <clinto92@gmail.com>
2023-02-03 08:02:02 +0530

    Merge branch 'master' of https://github.com/clinto92/wedding into dev

--- pages/terms-of-service/index.js
+++ pages/terms-of-service/index.js
@@ -1,8 +1,20 @@
+import { Button, Grid, Typography } from '@mui/material'
 import React from 'react'
 
 const TermsAndService = () => {
     return (
-        <div>TermsAndService</div>
+        <Grid container sx={{ padding: '2.1rem', margin: '2.1rem' }}>
+            <Typography variant='h5'>
+                Terms of service
+            </Typography>
+
+            <Typography>
+                The photographs are the property of solely Clinto Abraham Ayamkudiyil & Chippy N Thomas. These are not available for any marketing purpose. If anyone wants to download the photos, kindly contact us.
+            </Typography>
+            <Button>
+                Contact Us
+            </Button>
+        </Grid>
     )
 }
 
